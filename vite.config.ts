3b33010365237ac1db211d8f1a3569c23a278d24
Issam @TAGLAB <58403937+TAGLAB-Analytics@users.noreply.github.com>
2026-04-21 13:43:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,3 +7,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig();
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/taglab-org/',
+})
